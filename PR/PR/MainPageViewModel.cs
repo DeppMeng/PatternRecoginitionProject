fab966c75cf9ee2608e688cae8be8f66d7b8f0f5
DeppMeng <depp_meng@bitbucket.org>
2019-05-14 15:57:31 +0800

    added basic ui design

---
+++ PR/PR/MainPageViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Meowtrix.ComponentModel;
+using Windows.UI.Xaml;
+using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+using Windows.Storage;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Controls;
+
+namespace PR
+{
+    public class MainPageViewModel : NotificationObject
+    {
+        #region ViewModel Definitions
+        private ObservableCollection<string> classifierSelect = new ObservableCollection<string>();
+        public ObservableCollection<string> ClassifierSelect
+        {
+            get => classifierSelect;
+            set { classifierSelect = value; OnPropertyChanged(); }
+        }
+
+        private int classifierSelectIndex = 0;
+        public int ClassifierSelectIndex
+        {
+            get => classifierSelectIndex;
+            set { classifierSelectIndex = value; OnPropertyChanged(); }
+        }
+
+        private double testAccuracy = 0;
+        public double TestAccuracy
+        {
+            get => testAccuracy;
+            set { testAccuracy = value; OnPropertyChanged(); }
+        }
+
+        private double trainAccuracy = 0;
+        public double TrainAccuracy
+        {
+            get => trainAccuracy;
+            set { trainAccuracy = value; OnPropertyChanged(); }
+        }
+
+        #endregion
+
+
+    }
+}
--- PR/PR/obj/x86/Debug/App.g.i.cs
+++ PR/PR/obj/x86/Debug/App.g.i.cs
@@ -17,7 +17,7 @@ namespace PR
     /// </summary>
     public static class Program
     {
-        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.Windows.UI.Xaml.Build.Tasks"," 10.0.17.0")]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.Windows.UI.Xaml.Build.Tasks"," 10.0.16.0")]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         static void Main(string[] args)
         {
@@ -28,15 +28,15 @@ namespace PR
 
     partial class App : global::Windows.UI.Xaml.Application
     {
-        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.Windows.UI.Xaml.Build.Tasks"," 10.0.17.0")]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.Windows.UI.Xaml.Build.Tasks"," 10.0.16.0")]
         private bool _contentLoaded;
         /// <summary>
         /// InitializeComponent()
         /// </summary>
-        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.Windows.UI.Xaml.Build.Tasks"," 10.0.17.0")]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.Windows.UI.Xaml.Build.Tasks"," 10.0.16.0")]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         public void InitializeComponent()
-        {
+         {
             if (_contentLoaded)
                 return;
 
